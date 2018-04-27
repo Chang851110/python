ccbc116f64205fe415d54988f166467991fbdcaa
Chang851110 <kevin97563@gmail.com>
2018-04-27 17:22:02 +0800

    learning basis

--- first.py
+++ first.py
@@ -1,1 +1,26 @@
 print("Hello")
+print("Hello","fuck","you")
+print("hello","fuck",sep="-")
+print("hello","World",sep="$")
+
+str1="I am a cool guy."
+print(str1)
+print(len(str1))
+print(str1[3])
+print(str1[1:5])
+print(str1[8:])
+print(str1[:13])
+print(str1)
+print(str1[-1:5])
+print("{0} {1}".format(100,200))
+print("num1: {0} num2:{1}".format(a,b))
+print("{0:10}{1:10}".format(200,300))
+print("{0:7}{1:7}{2:7}.format()")
+
+
+
+#git status
+#git add .
+
+#git commit -m "123"
+#git push origin master
---
+++ test.py
@@ -0,0 +1,14 @@
+def test(1)
+str = "Stone Campus"
+print(len(str))
+#這個字串的長度
+print(str[6])
+#位置 6 的字母 (使用 [])
+print(str[3:9])
+#從位置 3 到 9 的子字串 "ne Camp" (使用util.slice())
+print(str[3:])
+#從位置 3 到 最後的子字串 "ne Campus" (使用util.slice())
+print(str[:10])
+#從位置 0 到 9 的子字串 "Stone Camp"  (使用util.slice())
+
+#請寫一個 test() 函數來測試你的程式.
